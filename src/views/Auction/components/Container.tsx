5c88f9bc3cf208cef45e59c54f17a96d5b054ac7
Harambe-Nakamoto <haramben@protonmail.com>
2022-03-16 13:07:22 -0500

    chore: Fix auction height

--- src/views/Auction/components/Container.tsx
+++ src/views/Auction/components/Container.tsx
@@ -2,7 +2,6 @@ import styled from 'styled-components'
 
 const Container = styled.div`
   background: ${({ theme }) => theme.colors.background};
-  height: calc(100vh - 64px);
   min-height: 100vh;
   overflow: hidden;
   position: relative;
