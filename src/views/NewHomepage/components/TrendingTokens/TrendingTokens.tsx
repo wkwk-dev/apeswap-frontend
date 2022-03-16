4a40f93708ca2dcd84d15d68720cd0e7c860b126
Harambe-Nakamoto <haramben@protonmail.com>
2022-03-16 16:15:06 -0500

    chore: Fix token link

--- src/views/NewHomepage/components/TrendingTokens/TrendingTokens.tsx
+++ src/views/NewHomepage/components/TrendingTokens/TrendingTokens.tsx
@@ -5,6 +5,7 @@ import useActiveWeb3React from 'hooks/useActiveWeb3React'
 import { Flex, Text, Skeleton } from '@apeswapfinance/uikit'
 import useIntersectionObserver from 'hooks/useIntersectionObserver'
 import { CHAIN_ID } from 'config/constants/chains'
+import { Link } from 'react-router-dom'
 import { useFetchHomepageTokenStats, useHomepageTokenStats } from 'state/hooks'
 import track from 'utils/track'
 import { Container, Title, TokenContainer, TrendingTokensWrapper } from './styles'
@@ -67,59 +68,59 @@ const TrendingTokens: React.FC = () => {
               <>
                 {tokens?.slice(tokenDisplayRange.tokenStartIndex, tokenDisplayRange.tokenEndIndex)?.map((token, i) => {
                   return (
-                    <a href={`/swap/?outputCurrency=${token?.contractAddress}`} key={token.contractAddress}>
-                      <TokenContainer
-                        key={token?.tokenTicker}
-                        active={i >= tokenDisplayRange.tokenStartIndex && i < tokenDisplayRange.tokenEndIndex}
-                        onClick={() =>
-                          track({
-                            event: 'tokenClick',
-                            chain: chainId,
-                            data: {
-                              token: token?.tokenTicker,
-                              variation: token?.percentChange,
-                              category: selectedCat,
-                            },
-                          })
-                        }
-                      >
-                        <Flex>
-                          <img
-                            src={token?.logoUrl}
-                            alt={token?.tokenTicker}
-                            style={{
-                              height: '36px',
-                              width: '36px',
-                              borderRadius: '50px',
-                            }}
-                          />
+                    <TokenContainer
+                      as={Link}
+                      to={`/swap/?outputCurrency=${token?.contractAddress}`}
+                      key={token?.tokenTicker}
+                      active={i >= tokenDisplayRange.tokenStartIndex && i < tokenDisplayRange.tokenEndIndex}
+                      onClick={() =>
+                        track({
+                          event: 'tokenClick',
+                          chain: chainId,
+                          data: {
+                            token: token?.tokenTicker,
+                            variation: token?.percentChange,
+                            category: selectedCat,
+                          },
+                        })
+                      }
+                    >
+                      <Flex>
+                        <img
+                          src={token?.logoUrl}
+                          alt={token?.tokenTicker}
+                          style={{
+                            height: '36px',
+                            width: '36px',
+                            borderRadius: '50px',
+                          }}
+                        />
+                      </Flex>
+                      <Flex flexDirection="column" ml="10px" style={{ height: '36px', width: '100px' }}>
+                        <Flex
+                          alignItems="center"
+                          justifyContent="space-between"
+                          style={{ width: '100%', height: '50%' }}
+                        >
+                          <Text fontSize="12px">{token?.tokenTicker}</Text>
+                          <Text fontSize="12px" color={token?.percentChange < 0 ? 'red' : 'green'}>
+                            {token?.percentChange > 0 && '+'}
+                            <CountUp end={token?.percentChange * 100} decimals={2} duration={1.5} />%
+                          </Text>
                         </Flex>
-                        <Flex flexDirection="column" ml="10px" style={{ height: '36px', width: '100px' }}>
-                          <Flex
-                            alignItems="center"
-                            justifyContent="space-between"
-                            style={{ width: '100%', height: '50%' }}
-                          >
-                            <Text fontSize="12px">{token?.tokenTicker}</Text>
-                            <Text fontSize="12px" color={token?.percentChange < 0 ? 'red' : 'green'}>
-                              {token?.percentChange > 0 && '+'}
-                              <CountUp end={token?.percentChange * 100} decimals={2} duration={1.5} />%
-                            </Text>
-                          </Flex>
-                          <Flex alignItems="center" style={{ width: '100%', height: '50%' }}>
-                            <Text>
-                              $
-                              <CountUp
-                                end={token?.tokenPrice}
-                                decimals={token?.tokenPrice > 1 ? 2 : 4}
-                                duration={1.5}
-                                separator=","
-                              />
-                            </Text>
-                          </Flex>
+                        <Flex alignItems="center" style={{ width: '100%', height: '50%' }}>
+                          <Text>
+                            $
+                            <CountUp
+                              end={token?.tokenPrice}
+                              decimals={token?.tokenPrice > 1 ? 2 : 4}
+                              duration={1.5}
+                              separator=","
+                            />
+                          </Text>
                         </Flex>
-                      </TokenContainer>
-                    </a>
+                      </Flex>
+                    </TokenContainer>
                   )
                 })}
               </>
